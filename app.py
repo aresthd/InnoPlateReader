cedca9de6990f0132251f9391bcaebbdd5a3a171
AcilRestu12 <arieskarestu214@gmail.com>
2023-12-18 14:36:56 +0700

    create read_plate and result page

--- app.py
+++ app.py
@@ -15,5 +15,13 @@ def login():
 def register():
 	return render_template("register.html")
 
+@app.route("/read_plate")
+def read_plate():
+	return render_template("read_plate.html")
+
+@app.route("/result")
+def result():
+	return render_template("result.html")
+
 if __name__ == '__main__':
 	app.run(debug=True)
