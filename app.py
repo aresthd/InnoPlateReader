ba121f7a8d23c1e2a4c88a417c6b587c5b72821f
AcilRestu12 <arieskarestu214@gmail.com>
2024-01-04 21:37:24 +0700

    add feature predict on read plate page

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_dropzone import Dropzone
 from werkzeug.utils import secure_filename
 import os
+import cv2
+import imutils
+import easyocr
+from ultralytics import YOLO
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -18,19 +22,99 @@ dropzone = Dropzone(app)
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 # app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-FILEPATH_READ_PLATE = None
+FILEPATH_PREDICT = {
+    'filename' : None,
+    'original' : None,
+    'object_detection' : None,
+    'crop_object' : None,
+    'text_detection' : None
+}
+
+MODEL_YOLO = "D:/Coding/Projects/number-plate-recognition/model/best.onnx"
+
+
+# Load a model
+model = YOLO(MODEL_YOLO)
+
+# Predict
+def predict(path_image, filename):
+    basedirPredict = 'static\images\predict'
+    
+    # Load image
+    img = cv2.imread(path_image)
+    imgSize = img.shape[0:2]
+
+    # Predict plat
+    results = model(path_image, conf=0.1)
+    results = results[0]
+    x1, y1, x2, y2 = None, None, None, None
+    
+    # Draw rectangle
+    for result in results.boxes.data.tolist():
+        x1, y1, x2, y2, score, classId = result
+        croppedImage = img[int(y1):int(y2), int(x1):int(x2)]
+        # print(results.names[int(classId)].upper())
+    
+    # Simpan hasil crop
+    pathImgCrop = os.path.join(basedirPredict, 'Crop_' + filename)
+    cv2.imwrite(pathImgCrop, croppedImage)
+    
+    # Melakukan text recognition dengan easy ocr
+    reader = easyocr.Reader(['en'])
+    result = reader.readtext(croppedImage)
+    
+    # Define Font
+    font = cv2.FONT_HERSHEY_SIMPLEX     # Jenis font
+    fontScale = 3                      # Skala font
+    fontThickness = 5                  # Ketebalan font
+    fontColor = (0, 255, 0)            # Warna biru (BGR)
+    
+    # Mengambil text hasil deteksi OCR
+    textToWrite = " ".join([res[1] for res in result])
+
+    # Gambar rectangle box
+    imgDrawBox = cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), fontColor, fontThickness)  # fontColor adalah warna merah
+    pathImgOb = os.path.join(basedirPredict, 'OB_' + filename)
+    cv2.imwrite(pathImgOb, imgDrawBox)
+    
+    # Tulis kata
+    imgDrawText = cv2.putText(imgDrawBox, textToWrite, (int(x1), int(y1) - 10), font, fontScale, fontColor, fontThickness)  # fontColor adalah warna merah
+    pathImgTD = os.path.join(basedirPredict, 'TD_' + filename)
+    cv2.imwrite(pathImgTD, imgDrawText)
+    
+    set_filepath_result(pathImgOb, pathImgCrop, pathImgTD)
+    return textToWrite
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def set_filepath_none():
+    global FILEPATH_PREDICT
+    FILEPATH_PREDICT['filename'] = None
+    FILEPATH_PREDICT['original'] = None
+    FILEPATH_PREDICT['object_detection'] = None
+    FILEPATH_PREDICT['crop_object'] = None
+    FILEPATH_PREDICT['text_detection'] = None
+
+def set_filepath_original(path, filename):
+    global FILEPATH_PREDICT
+    FILEPATH_PREDICT['filename'] = filename
+    FILEPATH_PREDICT['original'] = path
            
-def set_filepath_read(path):
-    global FILEPATH_READ_PLATE
-    FILEPATH_READ_PLATE = path
-           
-def get_filepath_read():
-    global FILEPATH_READ_PLATE
-    return FILEPATH_READ_PLATE
+def get_filepath_original():
+    global FILEPATH_PREDICT
+    return FILEPATH_PREDICT['original'], FILEPATH_PREDICT['filename']
+
+def set_filepath_result(pathOB, pathCrop, pathTD):
+    global FILEPATH_PREDICT
+    FILEPATH_PREDICT['object_detection'] = pathOB
+    FILEPATH_PREDICT['crop_object'] = pathCrop
+    FILEPATH_PREDICT['text_detection'] = pathTD
+    
+def get_filepath_result():
+    global FILEPATH_PREDICT
+    return FILEPATH_PREDICT['object_detection'], FILEPATH_PREDICT['crop_object'], FILEPATH_PREDICT['text_detection']
 
 @app.route("/")
 @app.route("/index")
@@ -49,11 +133,11 @@ def register():
 def read_plate():
     # dropzone.config['MAX_FILES'] = 1
     # dropzone.config['DROPZONE_MAX_FILES'] = 1
-    filepath = get_filepath_read()
+    filepath, filename = get_filepath_original()
     if filepath != None:
         print('\n Mengakses read plate dengan filepath...\n')
         print(f'\n filepath : {filepath}...\n')
-        return render_template("read_plate.html", filepath=filepath)
+        return render_template("read_plate.html", filepath=filepath, filename=filename)
     else:
         print('\n Mengakses read plate langsung...\n')
         return render_template("read_plate.html")
@@ -66,7 +150,7 @@ def upload():
             print('\n Proses dimulai...\n')
             filename = secure_filename(f.filename)
             filepath = os.path.join(app.config['UPLOADED_FOLDER'], filename)
-            set_filepath_read(filepath)
+            set_filepath_original(filepath, filename)
             print('\n Path berhasil diubah...\n')
             f.save(filepath)
             print('\n File berhasil disimpan...\n')
@@ -84,12 +168,15 @@ def upload():
 def result():
     print('\n--------------')
     print('\n\n Mengakses /result....\n')
-    filepath = get_filepath_read()
+    filepath, filename = get_filepath_original()
     if filepath != None and request.form['start-process-read']:
         print('\n\nStart Process Data....\n')
         start_process_read = request.form['start-process-read']
         print(f'\n filepath : {filepath}...\n')
-        return render_template('result.html', filepath=filepath, start_process_read=start_process_read)
+        print(f'\n Start Predict Image...\n')
+        result_text_plate = predict(filepath, filename)
+        pathOB, pathCrop, pathTD = get_filepath_result()
+        return render_template('result.html', filepath=filepath, filename=filename, pathOB=pathOB, pathCrop=pathCrop, pathTD=pathTD, result_text_plate=result_text_plate, start_process_read=start_process_read)
     else:
         print('\n\n Mengembalikkan ke halaman read_plate....\n')
         return redirect('/read_plate')
