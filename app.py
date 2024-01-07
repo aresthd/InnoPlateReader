c577ef4873ae6754c08a015f1d95497525e988a3
AcilRestu12 <arieskarestu214@gmail.com>
2024-01-08 00:52:27 +0700

    create score page and, add feature train and save model

--- app.py
+++ app.py
@@ -15,14 +15,15 @@ app = Flask(__name__)
 app.secret_key = "secret key"
 app.config.update(
     UPLOADED_FOLDER_PREDICT = os.path.join('static', 'uploads'),
-    UPLOADED_FOLDER_TRAIN = os.path.join('static', 'images', 'train'),
+    UPLOADED_FOLDER_TRAIN_IMG = os.path.join('static', 'images', 'train'),
+    UPLOADED_FOLDER_TRAIN_LABEL = os.path.join('static', 'labels', 'train'),
     DROPZONE_TIMEOUT = 5 * 60 * 1000,
     DROPZONE_ADD_REMOVE_LINKS = True
 )
 dropzone = Dropzone(app)
 
 # UPLOAD_FOLDER = "\static\upload"
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'txt'])
 # app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 FILEPATH_PREDICT = {
@@ -40,8 +41,16 @@ FILEPATH_TRAIN = {
     'model' : None
 }
 
+RESULT_TRAIN = {
+    'model_name' : None,
+    'model' : None,
+    'accuracy' : None, 
+    'precision' : None, 
+    'recall' : None
+}
+
 MODEL_YOLO = "D:/Coding/Projects/number-plate-recognition/model/best.pt"
-CONFIG = "D:/Coding/Projects/New folder/number-plate-recognition/static/config.yaml"
+CONFIG = "D:/Coding/Projects/number-plate-recognition/static/config.yaml"
 
 
 # Load a model
@@ -51,6 +60,8 @@ model = YOLO(MODEL_YOLO)
 def train(epoch, confidence):
     global model
     basedirTraining = os.path.join('static', 'training')
+    confidence = confidence / 100
+
     date = f'{str(datetime.now().day)}_{str(datetime.now().month)}_{str(datetime.now().year)}'
     time = f'{str(datetime.now().hour)}_{str(datetime.now().minute)}_{str(datetime.now().second)}'
     name = f'{date}_{time}'
@@ -72,9 +83,10 @@ def train(epoch, confidence):
     fp = cf[0][1]
     fn = cf[1][0]
     tn = cf[1][1]
-    accuracy = (tp + tn) / (tp+fp+fn+tn)
-    precision = tp / (tp+fp)
-    recall = tp /(tp+fn)
+    accuracy = ((tp + tn) / (tp+fp+fn+tn)) * 100
+    precision = (tp / (tp+fp)) * 100
+    recall = (tp /(tp+fn)) * 100
+    set_res_train(name, model, accuracy, precision, recall)
     
     return model, accuracy, precision, recall
     
@@ -124,14 +136,21 @@ def predict(path_image, filename):
     pathImgTD = os.path.join(basedirPredict, 'TD_' + filename)
     cv2.imwrite(pathImgTD, imgDrawText)
     
-    set_filepath_result(pathImgOb, pathImgCrop, pathImgTD)
+    set_path_predict_result(pathImgOb, pathImgCrop, pathImgTD)
     return textToWrite
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-def set_filepath_none():
+def get_file_extension(file_name):
+    try:
+        file_extension = file_name.split('.')[-1]
+        return file_extension.lower() 
+    except IndexError:
+        return None
+
+def set_path_none():
     global FILEPATH_PREDICT
     FILEPATH_PREDICT['filename'] = None
     FILEPATH_PREDICT['original'] = None
@@ -139,22 +158,22 @@ def set_filepath_none():
     FILEPATH_PREDICT['crop_object'] = None
     FILEPATH_PREDICT['text_detection'] = None
 
-def set_filepath_original(path, filename):
+def set_path_predict_original(path, filename):
     global FILEPATH_PREDICT
     FILEPATH_PREDICT['filename'] = filename
     FILEPATH_PREDICT['original'] = path
            
-def get_filepath_original():
+def get_path_predict_original():
     global FILEPATH_PREDICT
     return FILEPATH_PREDICT['original'], FILEPATH_PREDICT['filename']
 
-def set_filepath_result(pathOB, pathCrop, pathTD):
+def set_path_predict_result(pathOB, pathCrop, pathTD):
     global FILEPATH_PREDICT
     FILEPATH_PREDICT['object_detection'] = pathOB
     FILEPATH_PREDICT['crop_object'] = pathCrop
     FILEPATH_PREDICT['text_detection'] = pathTD
     
-def get_filepath_result():
+def get_path_predict_result():
     global FILEPATH_PREDICT
     return FILEPATH_PREDICT['object_detection'], FILEPATH_PREDICT['crop_object'], FILEPATH_PREDICT['text_detection']
 
@@ -164,6 +183,27 @@ def set_path_train(pathRes, pathVal, pathPlot, pathModel):
     FILEPATH_TRAIN['val'] = pathVal
     FILEPATH_TRAIN['plot'] = pathPlot
     FILEPATH_TRAIN['model'] = pathModel
+
+def set_res_train(model_name, model, accuracy, precision, recall):
+    global RESULT_TRAIN
+    RESULT_TRAIN['model_name'] = model_name + '.pt'
+    RESULT_TRAIN['model'] = model
+    RESULT_TRAIN['accuracy'] = accuracy
+    RESULT_TRAIN['precision'] = precision
+    RESULT_TRAIN['recall'] = recall
+
+def get_path_train_plot():
+    global FILEPATH_TRAIN
+    return FILEPATH_TRAIN['plot']
+
+def get_train_model():
+    global RESULT_TRAIN
+    global FILEPATH_TRAIN
+    return RESULT_TRAIN['model_name'], FILEPATH_TRAIN['model']
+
+def get_res_train_acc():
+    global RESULT_TRAIN
+    return RESULT_TRAIN['accuracy']
 
 @app.route("/")
 @app.route("/index")
@@ -182,7 +222,7 @@ def register():
 def read_plate():
     # dropzone.config['MAX_FILES'] = 1
     # dropzone.config['DROPZONE_MAX_FILES'] = 1
-    filepath, filename = get_filepath_original()
+    filepath, filename = get_path_predict_original()
     if filepath != None:
         print('\n Mengakses read plate dengan filepath...\n')
         print(f'\n filepath : {filepath}...\n')
@@ -195,20 +235,24 @@ def read_plate():
 def upload():
     if request.method == 'POST':
         f = request.files.get('file')
-        if f and allowed_file(f.filename):
-            print('\n Proses dimulai...\n')
-            filename = secure_filename(f.filename)
-            filepath = os.path.join(app.config['UPLOADED_FOLDER_PREDICT'], filename)
-            set_filepath_original(filepath, filename)
-            print('\n Path berhasil diubah...\n')
-            f.save(filepath)
-            print('\n File berhasil disimpan...\n')
-            print('\n--------------')
-            # print(UPLOAD_FOLDER)
-            print(filepath)
-            print(filename)
-            print('\n\n')
-            # flash('File successfully uploaded')
+        filename = secure_filename(f.filename)
+        if f and allowed_file(filename):
+            print('\n Mengecek ekstensi file...\n')
+            ext = get_file_extension(filename)
+            if ext != "txt":
+                print('\n Proses dimulai...\n')
+                filepath = os.path.join(app.config['UPLOADED_FOLDER_PREDICT'], filename)
+                set_path_predict_original(filepath, filename)
+                print('\n Path berhasil diubah...\n')
+                f.save(filepath)
+                print('\n File berhasil disimpan...\n')
+                print('\n--------------')
+                # print(UPLOAD_FOLDER)
+                print(filepath)
+                print(filename)
+                print('\n\n')
+                # flash('File successfully uploaded')
+                return redirect("/read-plate")
             return redirect("/read-plate")
     else:
         return redirect("/read-plate")
@@ -217,14 +261,14 @@ def upload():
 def result():
     print('\n--------------')
     print('\n\n Mengakses /result....\n')
-    filepath, filename = get_filepath_original()
+    filepath, filename = get_path_predict_original()
     if filepath != None and request.form['start-process-read']:
         print('\n\nStart Process Data....\n')
         start_process_read = request.form['start-process-read']
         print(f'\n filepath : {filepath}...\n')
         print(f'\n Start Predict Image...\n')
         result_text_plate = predict(filepath, filename)
-        pathOB, pathCrop, pathTD = get_filepath_result()
+        pathOB, pathCrop, pathTD = get_path_predict_result()
         return render_template('result.html', filepath=filepath, filename=filename, pathOB=pathOB, pathCrop=pathCrop, pathTD=pathTD, result_text_plate=result_text_plate, start_process_read=start_process_read)
     else:
         print('\n\n Mengembalikkan ke halaman read_plate....\n')
@@ -234,9 +278,9 @@ def result():
 def download_result():
     print('\n--------------')
     print('\n\n Mengakses /download-result....\n')
-    filepath, filename = get_filepath_original()
+    filepath, filename = get_path_predict_original()
     if filepath != None:
-        pathOB, pathCrop, pathTD = get_filepath_result()
+        pathOB, pathCrop, pathTD = get_path_predict_result()
         filename = 'Result_' + filename
         return send_file(
             pathTD,
@@ -249,14 +293,14 @@ def download_result():
     
 @app.route("/read-again")
 def read_again():
-    set_filepath_none()
+    set_path_none()
     return redirect('/read-plate')
 
 @app.route("/upgrade-model")
 def upgrade_model():
     # dropzone.config['MAX_FILES'] = 1
     # dropzone.config['DROPZONE_MAX_FILES'] = 1
-    filepath, filename = get_filepath_original()
+    filepath, filename = get_path_predict_original()
     if filepath != None:
         print('\n Mengakses upgrade_model dengan filepath...\n')
         print(f'\n filepath : {filepath}...\n')
@@ -269,11 +313,20 @@ def upgrade_model():
 def upload_train():
     if request.method == 'POST':
         f = request.files.get('file')
-        if f and allowed_file(f.filename):
-            print('\n Proses dimulai...\n')
-            filename = secure_filename(f.filename)
-            filepath = os.path.join(app.config['UPLOADED_FOLDER_TRAIN'], filename)
-            f.save(filepath)
+        filename = secure_filename(f.filename)
+        if f and allowed_file(filename):
+            print('\n Mengecek ekstensi file...\n')
+            ext = get_file_extension(filename)
+            if ext == "txt":
+                print(f'\n File txt...\n')
+                print(f'\n Mulai menyimpan file {ext}...\n')
+                filepath = os.path.join(app.config['UPLOADED_FOLDER_TRAIN_LABEL'], filename)
+                f.save(filepath)
+            else:                
+                print(f'\n File bukan txt...\n')
+                print(f'\n Mulai menyimpan file {ext}...\n')
+                filepath = os.path.join(app.config['UPLOADED_FOLDER_TRAIN_IMG'], filename)
+                f.save(filepath)
             print('\n File berhasil disimpan...\n')
             print('\n--------------')
             # print(UPLOAD_FOLDER)
@@ -290,20 +343,41 @@ def train_model():
     print('\n--------------')
     print('\n\n Mengakses /train....\n')
     if request.form['start-train'] == 'True':
-        epoch = request.form['epoch']
-        confidence = request.form['confidence']
+        epoch = int(request.form['epoch'])
+        confidence = int(request.form['confidence'])
         print('\n\n Start Train Data....\n')
         model, accuracy, precision, recall = train(epoch, confidence)
-        
-        
-        return redirect('/')
+        plotTrain = get_path_train_plot()
+        return render_template('score.html', model=model, accuracy=accuracy, precision=precision, recall=recall, plotTrain=plotTrain)
     elif request.form['start-train'] == 'False':
         print('\n\n Mengembalikkan ke halaman upgrade model....\n')
         return redirect('/upgrade-model')
     else:
         print('\n\n Mengembalikkan ke halaman upgrade model....\n')
         return redirect('/upgrade-model')
-        
+
+    
+@app.route("/save-model")
+def save_model():
+    print('\n--------------')
+    print('\n\n Mengakses /save-model....\n')
+    modelName, pathModel = get_train_model()
+    if pathModel != None:
+        modelName = 'Model_' + modelName
+        return send_file(
+            pathModel,
+            download_name=modelName,
+            as_attachment=True,
+        )   
+    else:
+        print('\n\n Mengembalikkan ke halaman upgrade model....\n')
+        return redirect('/score')
+
+@app.route("/score")
+def score():
+    accuracy = get_res_train_acc()
+    plotTrain = get_path_train_plot()
+    return render_template('score.html', accuracy=accuracy, plotTrain=plotTrain)
 
 # @app.route("/result", methods=['GET', 'POST'])
 # def result():
